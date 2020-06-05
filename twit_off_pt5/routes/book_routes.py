ffc0eda10430555fea2e42d5caeedc03a94e940c
Erik Seguinte <erikseguinte@me.com>
2020-06-05 15:30:04 -0700

    Complete database api

--- twit_off_pt5/routes/book_routes.py
+++ twit_off_pt5/routes/book_routes.py
@@ -1,23 +1,18 @@
 from flask import Blueprint, jsonify, request, render_template
+from twit_off_pt5.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    records = Book.query.all()
+    books = parse_records(records)
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    records = Book.query.all()
+    books = parse_records(records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
@@ -27,7 +22,16 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
+
+    data = request.form
+    breakpoint()
+    
+    new_book = Book(title=data["title"], author_id=data["author_id"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+
     return jsonify({
         "message": "BOOK CREATED OK (TODO)",
         "book": dict(request.form)})
