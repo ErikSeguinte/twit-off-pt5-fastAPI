502dc7926aa8254bd685a77d9ef2d07392338e8d
Erik Seguinte <erikseguinte@me.com>
2020-06-05 13:19:13 -0700

    templates

--- twit_off_pt5/__init__.py
+++ twit_off_pt5/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '0.1.0'
+import jinja2
+from flask import Flask
+
+from twit_off_pt5.routes.home_routes import home_routes
+from twit_off_pt5.routes.book_routes import book_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(home_routes)
+    app.register_blueprint(book_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
