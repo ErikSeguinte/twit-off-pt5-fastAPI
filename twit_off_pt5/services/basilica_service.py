d651caf210989ee818a8ca211d3ffcf033a46bc2
Erik <spamfactorx01@gmail.com>
2020-06-07 13:57:23 -0700

    removes extra import

--- twit_off_pt5/services/basilica_service.py
+++ twit_off_pt5/services/basilica_service.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 key = os.getenv('basilica_key')
 
-import basilica
 
 with basilica.Connection(key) as c:
     embeddings = c.embed_sentences(["Hello world!", "How are you?"])
+    breakpoint()
     print(list(embeddings))  # [[0.8556405305862427, ...], ...]
