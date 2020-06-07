95694ae5a5c560d569df06c2af8d39f1484b862b
Erik <spamfactorx01@gmail.com>
2020-06-07 14:34:19 -0700

    formates code

--- twit_off_pt5/services/twitter_service.py
+++ twit_off_pt5/services/twitter_service.py
@@ -5,15 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("twitter_api")
-api_secret = os.getenv('twitter_secret')
-access = os.getenv('twitter_access')
-access_secret = os.getenv('twitter_access_secret')
+api_secret = os.getenv("twitter_secret")
+access = os.getenv("twitter_access")
+access_secret = os.getenv("twitter_access_secret")
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access, access_secret)
 
 api = tweepy.API(auth)
 
-user = api.get_user('primefactorx01')
+user = api.get_user("primefactorx01")
 print(type(user))
 breakpoint()
