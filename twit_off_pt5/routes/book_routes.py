5289d60b7f674e131278ee4edc4c7a86e58b69fd
Erik <spamfactorx01@gmail.com>
2020-06-08 10:53:58 -0700

    fixes quote

--- twit_off_pt5/routes/book_routes.py
+++ twit_off_pt5/routes/book_routes.py
@@ -3,11 +3,13 @@ from twit_off_pt5.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
+
 @book_routes.route("/books.json")
 def list_books():
     records = Book.query.all()
     books = parse_records(records)
     return jsonify(books)
+
 
 @book_routes.route("/books")
 def list_books_for_humans():
@@ -15,23 +17,22 @@ def list_books_for_humans():
     books = parse_records(records)
     return render_template("books.html", message="Here's some books", books=books)
 
+
 @book_routes.route("/books/new")
 def new_book():
     return render_template("new_book.html")
+
 
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
 
-    data = request.form
     breakpoint()
-    
+    data = request.form
+
     new_book = Book(title=data["title"], author_id=data["author_id"])
 
     db.session.add(new_book)
     db.session.commit()
 
-
-    return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
-        "book": dict(request.form)})
+    return jsonify({"message": "BOOK CREATED OK (TODO)", "book": dict(request.form)})
