f24ef563e4c2b6cd1449e526342084ebf9e3ab3c
Erik <spamfactorx01@gmail.com>
2020-06-09 20:24:01 -0700

    implements prediction model

--- twit_off_pt5/routes/stats_routes.py
+++ twit_off_pt5/routes/stats_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify, render_template
 
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 from twit_off_pt5.core.models import User, Tweet
 from twit_off_pt5.services.basilica_service import connection as basilica
@@ -13,9 +15,43 @@ def predict():
     # TODO
     data = request.form
     print(data)
+    user_1 = _get_user(data['user_1'])
+    user_1_embeddings = [tweet.embedding for tweet in user_1.tweets]
+    user_2 = _get_user(data['user_2'])
+    user_2_embeddings = [tweet.embedding for tweet in user_2.tweets]
+
+
+    embeddings = []
+    labels = []
+
+    for embedding in user_1_embeddings:
+        labels.append(user_1.screen_name)
+        embeddings.append(embedding)
+
+    for embedding in user_2_embeddings:
+        labels.append(user_2.screen_name)
+        embeddings.append(embedding)
+    classifier = make_pipeline(StandardScaler(), LogisticRegression())
+    classifier.fit(embeddings, labels)
+
+
+    new_embedding = basilica.embed_sentence(data['text'], model='twitter')
+    result = classifier.predict([new_embedding])
+
+    return str(result)
+
+
+
     return "TODO: PREDICTING"
 
 
 @stats_routes.route("/predict_form")
 def get_usernames():
     return render_template("predict_form.html")
+
+
+def _get_user(username):
+    user = User.query.filter(User.screen_name==username).one()
+    print(user)
+    return user
+
