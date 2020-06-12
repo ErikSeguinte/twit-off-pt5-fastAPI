d94a6512859b120b0242ce206658a707304bff65
Erik <spamfactorx01@gmail.com>
2020-06-11 17:19:52 -0700

    minor changes

--- twit_off_pt5/__init__.py
+++ twit_off_pt5/__init__.py
@@ -12,6 +12,7 @@ from twit_off_pt5.core.models import db, migrate
 
 # db_path = Path("web_app.db")
 # db_uri = "sqlite:/" + db_path.resolve().as_uri()[5:]
+
 db_uri = "postgres://wbakakfzhirkzk:85e2c6305a72c6aef469b01910aae3b04188511880434c08f5fd163ca81c41ea@ec2-35-169-254-43.compute-1.amazonaws.com:5432/df5orp7bpp1h3n"
 
 
