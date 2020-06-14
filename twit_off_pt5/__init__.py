4f5e6782dd4bd4a11bc244bf3f2d6c939b620e5a
Erik <spamfactorx01@gmail.com>
2020-06-14 10:41:20 -0700

    Add text describing how it works.

--- twit_off_pt5/__init__.py
+++ twit_off_pt5/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-    app.config["SQLALCHEMY_TRACK_MODIFICATINS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
